1e3442874176c7fec6376ee1e36ccc489b14c658
Yomariii <avashtamrakar740@gmail.com>
2024-03-13 12:55:42 +0545

    initial chat
    initial chat integration

---
+++ Backend/test/test/DataService/SharedDb.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Concurrent;
+using test.Models;
+
+namespace test.DataService
+{
+    public class SharedDb
+    {
+        private readonly ConcurrentDictionary<string, UserConnection> _connections = new();
+        public ConcurrentDictionary<string, UserConnection> connections => _connections;
+    }
+}
---
+++ Backend/test/test/Hubs/ChatHub.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+using test.Models;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using test.DataService;
+namespace test.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly SharedDb _shared;
+
+        public ChatHub(SharedDb shared) => _shared = shared;
+
+
+        public async Task JoinChat(UserConnection conn)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined.");
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection conn)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
+
+            _shared.connections[Context.ConnectionId] = conn;
+
+            await Clients.Group(conn.ChatRoom).SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined {conn.ChatRoom}.");
+        }
+
+        public async Task SendMessage(string msg)
+        {
+            if (_shared.connections.TryGetValue(Context.ConnectionId, out UserConnection conn))
+            {
+                await Clients.Group(conn.ChatRoom).SendAsync("ReceiveSpecificMessage", conn.Username, msg);
+            }
+        }
+
+    }
+}
